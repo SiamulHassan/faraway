0266042133267d808c854fb986f82fbb674a1add
SiamulHassan <syeam45@gmail.com>
2023-08-14 06:11:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import "./index.css";
 //   { id: 2, description: "Socks", quantity: 12, packed: false },
 //   { id: 3, description: "Charger", quantity: 1, packed: true },
 // ];
-
 // Array.from({ length: 10 }, (currELe, i) =>
 //   console.log("current:::", currELe, i)
 // );
-
+// console.log(Number(true));
 function App() {
   const [item, setItem] = useState([]);
   const getDataForm = (formData) => {
@@ -18,12 +17,20 @@ function App() {
   const handleDelet = (id) => {
     setItem((prev) => prev.filter((filteredItem) => filteredItem.id !== id));
   };
+  const handleCheck = (id) =>
+    setItem((prevItem) =>
+      prevItem.map((prevItem) =>
+        prevItem.id === id
+          ? { ...prevItem, packed: !prevItem.packed }
+          : prevItem
+      )
+    );
   return (
     <div className="App">
       <Logo />
       <Form getDataForm={getDataForm} />
-      <PackingList item={item} onDelet={handleDelet} />
-      <Stats />
+      <PackingList item={item} onDelet={handleDelet} onCheck={handleCheck} />
+      <Stats item={item} />
     </div>
   );
 }
@@ -70,21 +77,42 @@ function Form({ getDataForm }) {
     </form>
   );
 }
-function PackingList({ item, onDelet }) {
+function PackingList({ item, onDelet, onCheck }) {
+  const [sorted, setSorted] = useState("input");
+  let sortedVal;
+  if (sorted === "input") sortedVal = item;
+  if (sorted === "description")
+    sortedVal = item
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sorted === "checked")
+    sortedVal = item
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {item.map((item) => (
-          <Item item={item} onDelet={onDelet} key={item.id} />
+        {sortedVal.map((item) => (
+          <Item item={item} onDelet={onDelet} onCheck={onCheck} key={item.id} />
         ))}
       </ul>
+      <select value={sorted} onChange={(e) => setSorted(e.target.value)}>
+        <option value="input">Sort by input</option>
+        <option value="description">Sort by description</option>
+        <option value="checked">Sort by checked</option>
+      </select>
     </div>
   );
 }
-function Item({ item, onDelet }) {
+function Item({ item, onDelet, onCheck }) {
   return (
     <>
       <li>
+        <input
+          type="checkbox"
+          value={item.packed}
+          onChange={() => onCheck(item.id)}
+        />
         <span style={item.packed ? { textDecoration: "line-through" } : {}}>
           {item.quantity} {item.description}
         </span>
@@ -93,10 +121,29 @@ function Item({ item, onDelet }) {
     </>
   );
 }
-function Stats() {
+function Stats({ item }) {
+  if (!item.length)
+    return (
+      <p className="stats">
+        Please create some item and start packing to go 🚀
+      </p>
+    );
+
+  const numItems = item.length;
+  // jodi packed er value true thake then taile return korbe false hole korbe na
+  const numPackedItems = item.filter(
+    (filteredItem) => filteredItem.packed
+  ).length;
+  const percentage = Math.floor((numPackedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list and you already packed X (x%)</em>
+      <em>
+        {percentage === 100
+          ? `Your are ready to go ✈`
+          : ` You have ${numItems} items on your list and you already packed
+        ${numPackedItems} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
